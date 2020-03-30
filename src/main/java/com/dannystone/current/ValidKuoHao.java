b2d9cbcc752cc290103e8528ba25025a24462313
daniel <daniel.zhang@ipiaoniu.com>
2020-03-30 22:42:36 +0800

    valid括号

---
+++ src/main/java/com/dannystone/current/ValidKuoHao.java
@@ -0,0 +1,42 @@
+package com.dannystone.current;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: daniel
+ * @creed: keep it simple and stupid !
+ * @Time: 2020/3/30 7:50 PM
+ */
+public class ValidKuoHao {
+
+
+    public boolean isValid(String s) {
+        Map<String, String> pairs = new HashMap<>();
+        pairs.put("{", "}");
+        pairs.put("(", ")");
+        pairs.put("[", "]");
+
+        Stack<String> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (pairs.containsKey(aChar + "")) {
+                stack.push(aChar + "");
+            } else if (stack.size() == 0 || !pairs.get(stack.pop()).equals(aChar + "")) {
+                return false;
+            }
+
+        }
+
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+}
